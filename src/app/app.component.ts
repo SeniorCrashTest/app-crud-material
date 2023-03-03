3665ee4f6ce8cd225e9248c9b853ec2a70acd059
Yaresko Vlad <yareskofamily@gmail.com>
2023-03-03 15:25:34 +0200

    Устранение некорректного отображения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,6 @@ export class AppComponent implements OnInit {
 	applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
